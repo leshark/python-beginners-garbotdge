23429b599d9164c641d31300679d00f2336ad3b4
Pavel Durmanov <alban@national.shitposting.agency>
2019-03-14 22:17:23 +0200

    fix

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ telebot.logger.setLevel(logging.INFO)
 
 def validate_document(message):
     if message.document:
-        file_name = message.document.file_name
+        file_name = message.document.file_name or ''
         file_size = message.document.file_size
         return os.path.splitext(file_name)[-1] in config.EXTENSIONS and file_size <= config.MAX_FILE_SIZE
 
